dfc5dcfe424a078d061ae4330479328da8474eaf
Adarsh4775 <137145969+Adarsh4775@users.noreply.github.com>
2023-10-09 20:56:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,79 @@
 //your code here
+const input = document.createElement("input");
+input.id="input";
+input.setAttribute("type","text");
+document.body.appendChild(input);
+
+let buttons = document.querySelectorAll("button");
+let currentInput = " ";
+
+function UpdateInput(){
+	input.value = currentInput;
+}
+
+for(let i=0; i<=9; i++)
+	{
+		document.getElementById(`block${i}`).addEventListener("click",() =>{
+			currentInput += i;
+			UpdateInput();
+		});
+	}
+
+document.getElementById("dot").addEventListener("click",() =>{
+	if(!currentInput.includes("."))
+	{
+		currentInput += ".";
+		UpdateInput();
+	}
+});
+
+const operators = ["plus","minus","multiply","divide"];
+operators.forEach((operator)=>
+	             document.getElementById(operator).addEventListener("click", ()=>{
+					     currentInput += operator === "plus" ? "+" : operator === "minus" ? "-" : operator === "multiply" ? "*" : "/";
+      UpdateInput();
+						 });
+	                 )
+
+document.getElementById("clr").addEventListener("click", () =>{
+	            currentInput=" ";
+	            UpdateInput();
+});
+
+document.getElementById("ans").addEventListener("click", () =>{
+	try 
+		{
+			const result = eval(currentInput);
+			input.value = result === Infinity ? Infinity : isNaN(result) ? "NaN" : result;
+			currentInput = result.toString();
+		}
+	catch (error) {
+		input.value="error";
+		currentInput = " ";
+	}
+});
+
+document.getElementById("clear").addEventListener("click", () =>{
+	       currentInput = currentInput.slice(0, -1);
+	UpdateInput();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
