b81cbf2525301eebddeb2fb578edf2f426dcca59
Adarsh4775 <137145969+Adarsh4775@users.noreply.github.com>
2023-10-10 13:55:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,75 +1,24 @@
 //your code here
+const input = document.getElementById('input');
+const buttons = document.querySelectorAll('.buttons button');
 
-
-let currentInput = "";
-
-function UpdateInput(){
-	input.value = currentInput;
-}
-
-for(let i=0; i<=9; i++)
-	{
-		document.getElementById(`block${i}`).addEventListener("click",() =>{
-			currentInput += i;
-			UpdateInput();
-		});
-	}
-
-document.getElementById("dot").addEventListener("click",() =>{
-	if(!currentInput.includes("."))
-	{
-		currentInput += ".";
-		UpdateInput();
-	}
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        if (button.id === 'clr') {
+            input.value = '';
+        } else if (button.id === 'ans') {
+            try {
+                input.value = eval(input.value);
+                if (input.value === Infinity) {
+                    input.value = 'Infinity';
+                } else if (isNaN(input.value)) {
+                    input.value = 'NaN';
+                }
+            } catch (error) {
+                input.value = 'Error';
+            }
+        } else {
+            input.value += button.textContent;
+        }
+    });
 });
-
-const operators = ["plus","minus","multiply","divide"];
-operators.forEach((operator)=>
-	             document.getElementById(operator).addEventListener("click", ()=>{
-					     currentInput += operator === "plus" ? "+" : operator === "minus" ? "-" : operator === "multiply" ? "*" : "/";
-      UpdateInput();
-						 });
-	                 )
-
-document.getElementById("clr").addEventListener("click", () =>{
-	            currentInput="";
-	            UpdateInput();
-});
-
-document.getElementById("ans").addEventListener("click", () =>{
-	try 
-		{
-			const result = eval(currentInput);
-			input.value = result === Infinity ? Infinity : isNaN(result) ? "NaN" : result;
-			currentInput = result.toString();
-		}
-	catch (error) {
-		input.value="error";
-		currentInput = " ";
-	}
-});
-
-document.getElementById("clear").addEventListener("click", () =>{
-	       currentInput = currentInput.slice(0, -1);
-	UpdateInput();
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
